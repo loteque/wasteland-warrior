a424b7658e4370a1b8294dd1a280a27128cb4e96
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2023-02-21 22:44:49 -0800

    Add projectile and enemy damage and death
    Closes # 64

--- project/src/Projectile.gd
+++ project/src/Projectile.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 export var speed := 100
 export var angle := 0
+export var damage := 1
+export var penetration_max := 2
+
+var penetration_count = 0
 
 var velocity = Vector2(speed, 0).rotated(angle)
 
@@ -12,3 +16,15 @@ func update(speed: float = self.speed, angle: float = self.angle):
 	self.speed = speed
 	self.angle = angle
 	self.velocity = Vector2(speed, 0).rotated(angle)
+
+func on_hit_enemy():
+	penetration_count += 1
+	if penetration_count >= penetration_max:
+		die()
+	
+func die():
+	queue_free()
+
+func _on_Projectile_area_entered(area):
+	if area.is_in_group("Mobs"):
+		on_hit_enemy()
