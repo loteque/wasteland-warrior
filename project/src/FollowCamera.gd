5812395048a16d2dc683bafb3c465d501236b8cf
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2023-02-25 00:49:30 -0800

    Stop UI from flipping when character faces left
    Creates a FollowCamera which can follow targets.
    FollowCamera follows the PlayerController to prevent UI flip.
    Also removes out of date test scenes.
    
    Closes #88

--- project/src/FollowCamera.gd
+++ project/src/FollowCamera.gd
@@ -1,10 +1,17 @@
 extends Camera2D
 
+export var target_node_path: NodePath
 export var update_period := 15
 
+onready var target: Node2D = get_node(target_node_path)
+
 func _physics_process(delta: float) -> void:
+	follow(target)
 	if is_update_frame():
 		Signals.emit_signal("updated_camera_pos", global_position)
+
+func follow(target: Node2D):
+	transform.origin = target.transform.origin
 
 func is_update_frame():
 	return Engine.get_physics_frames() % update_period == 0
