bceb68a77f41ac452e3e45d43d981fd8305157ce
loteque <andrewandroid@gmail.com>
2023-02-25 03:35:59 +0100

    fix regressions

--- project/src/PlayerController.gd
+++ project/src/PlayerController.gd
@@ -31,14 +31,14 @@ func _physics_process(delta):
 		
 	if Input.is_action_pressed("ui_left"):
 		motion.x -= 1
-
+		face_left()
 
 	if Input.is_action_pressed("ui_right"):
 		motion.x += 1
 		face_right()
 
-	if !motion.x == 0 and !motion.y == 0:
-		sprite.play("run")
+	if motion.x == 0 and motion.y == 0:
+		sprite.play("idle")
 
 	else:
 		sprite.play("run")
