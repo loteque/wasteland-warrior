f9e87fa5264cce7d84da1d9f6d9343b2eb0ec67a
loteque <andrewandroid@gmail.com>
2023-10-07 23:42:27 +0200

    Refactor `CountComponent` to `KeyValueComponent`

---
+++ project/src/ui/components/KeyValueComponent.gd
@@ -0,0 +1,6 @@
+extends Control
+class_name KeyValueComponent
+
+@export var game_stats: GameStatsResource
+@export var key: Label
+@export var value: Label
--- project/src/ui/components/UICountComponent.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-class_name CountComponent
-
-@export var game_stats: GameStatsResource
-@export var count_name_node: Label
-@export var count_amount_node: Label
