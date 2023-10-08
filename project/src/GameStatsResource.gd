c881c8839ea5f5ad1007f86d930d6f16aba4331c
loteque <andrewandroid@gmail.com>
2023-10-08 05:52:59 +0200

    Remove dependancy from `PickupComponent` on `GameStatRescource`

--- project/src/GameStatsResource.gd
+++ project/src/GameStatsResource.gd
@@ -3,5 +3,11 @@ class_name GameStatsResource
 
 @export var cap_count := 0
 
+func _init():
+    Signals.connect("cap_collected", Callable(self, "_on_cap_collected"))
+
 func update_cap_count(count: int):
     cap_count = cap_count + count
+
+func _on_cap_collected():
+    update_cap_count(1)
