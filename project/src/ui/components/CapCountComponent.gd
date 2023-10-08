944f60983f2e5c0e21eac4992d2344a62633ebf7
loteque <andrewandroid@gmail.com>
2023-10-08 11:05:30 +0200

    Use `set_value_text`

--- project/src/ui/components/CapCountComponent.gd
+++ project/src/ui/components/CapCountComponent.gd
@@ -4,4 +4,4 @@ func _ready():
 	Signals.connect("cap_collected", Callable(self, "_on_cap_collected"))
 
 func _on_cap_collected():
-	value.text = str(game_stats.cap_count)
+	set_value_text(resource, "cap_count")
