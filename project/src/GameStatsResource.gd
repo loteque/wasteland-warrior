95fa3ac2debc5f0ef28419a3a1547a2e1635b4ce
loteque <andrewandroid@gmail.com>
2023-10-08 10:50:57 +0200

    Add `get_value()` to GameStatsResource
    `get_value()` is a general function to get the value of a property in GameStatsResource.
    It provides an abstracted interface to safely get, at runtime, any property value of the GameStatsResource Class.

--- project/src/GameStatsResource.gd
+++ project/src/GameStatsResource.gd
@@ -6,6 +6,11 @@ class_name GameStatsResource
 func _init():
     Signals.connect("cap_collected", Callable(self, "_on_cap_collected"))
 
+func get_value(property_name: String):
+    var property_value = get(property_name)
+    if property_value:
+        return property_value
+
 func update_cap_count(count: int):
     cap_count = cap_count + count
 
