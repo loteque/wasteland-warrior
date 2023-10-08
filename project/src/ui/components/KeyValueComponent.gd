a4238fd2bb622622a6daca8fb72d5a8b8c78166b
loteque <andrewandroid@gmail.com>
2023-10-08 10:58:33 +0200

    Add `set_value_text()` to KeyValueComponent
    This sets the text element of the value label.
    It provides a general runtime interface to set the text `String` of `value_label` to the value of a property in any given `Resource`.

--- project/src/ui/components/KeyValueComponent.gd
+++ project/src/ui/components/KeyValueComponent.gd
@@ -1,6 +1,11 @@
-extends Control
 class_name KeyValueComponent
+extends Control
 
-@export var game_stats: GameStatsResource
+@export var resource: Resource
 @export var key: Label
 @export var value: Label
+
+## Sets the text element of the value label given a resource (res) and var member name (res_key) of the resource.
+func set_value_text(res: Resource, res_key: String):
+	if res and res.has_method("get_value"):
+		value.text = str(res.get_value(res_key))
