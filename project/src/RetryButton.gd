5ba28e2c9e430bd0c3e8cb83a355c7208366499b
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2023-10-09 15:20:41 -0700

    Add `RetryButton` and `QuitButton` function
    * When on the `GameOverDialog`, pressing `RetryButton` reloads level
    * When on `GameOverDialog`, press `QuitButton` loads the `TitleScreen`
    * When reloading or leaving level, remove all mobs
    * Update "mob" group name to correct "Mob"
    
    Mob code duplication expected to be resolved in #124
    
    Closes #120

--- project/src/RetryButton.gd
+++ project/src/RetryButton.gd
@@ -1,5 +1,11 @@
 extends TextureButton
 
 func _on_pressed():
-#	get_tree().reload_current_scene()
-	pass
+	remove_mobs()
+	get_tree().reload_current_scene()
+
+func remove_mobs():
+	var mobs = get_tree().get_nodes_in_group("Mobs")
+	for mob in mobs:
+		mob.set_physics_process(false)
+		mob.queue_free()
