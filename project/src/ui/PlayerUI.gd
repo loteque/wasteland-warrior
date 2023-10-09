3a036895b1f5577c76e13e4bc681c29fd2eb226a
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2023-10-09 10:28:05 -0700

    When player dies, show `GameOverDialog`
    Used a smaller container for `GameOverDialog`
    
    * Shows "Game Over" at top
    * Shows `RetryButton` and `QuitButton`
    * `player_died` global signal makes game over appear.
    * Retry button takes focus when menu appears.
    
    Part of #120

---
+++ project/src/ui/PlayerUI.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var game_over_dialog: Control = $GameOverDialog
+
+func _ready():
+	Signals.connect("player_died", Callable(self, "_on_player_died"))
+
+func show_game_over_dialog():
+	game_over_dialog.show()
+
+func _on_player_died():
+	show_game_over_dialog()
