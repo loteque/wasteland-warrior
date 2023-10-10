8fb9e7d732d328f9240aa669492bf70603d5a475
loteque <andrewandroid@gmail.com>
2023-10-11 00:02:08 +0200

    Add `set_key_text_from_resource` to `KeyValueComponent`, Update CapCountComponent
    * Closes #138

--- project/src/ui/components/CapCountComponent.gd
+++ project/src/ui/components/CapCountComponent.gd
@@ -2,6 +2,7 @@ extends KeyValueComponent
 
 func _ready():
 	Signals.connect("cap_collected", Callable(self, "_on_cap_collected"))
+	set_key_text_from_resource(resource, "cap_count")
 
 func _on_cap_collected():
 	set_value_text_from_resource(resource, "cap_count")
--- project/src/ui/components/KeyValueComponent.gd
+++ project/src/ui/components/KeyValueComponent.gd
@@ -10,5 +10,10 @@ func set_value_text_from_resource(res: Resource, res_key: String):
 	if res and res.has_method("get_value"):
 		value.text = str(res.get_value(res_key))
 
+## Sets the text element of the key label given a resource (res) and var member name (res_key) of the resource.
+func set_key_text_from_resource(res: Resource, res_key: String):
+	if res and res_key in res:
+		key.text = str(res_key)
+
 func set_value_text(text: String):
 	value.text = text
