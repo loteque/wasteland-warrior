bf32365bdf21e14c6cbc122753013df481270b9b
loteque <andrewandroid@gmail.com>
2023-10-11 05:10:05 +0200

    InvincibilityComponent prevents player from damage

--- project/src/HurtBox.gd
+++ project/src/HurtBox.gd
@@ -1,12 +1,38 @@
 extends Area2D
+class_name HurtBox
 
 @export var hurt_group: String
 
+#Is it better to directly access the sibling instead of using a signal?
+#I noticed it makes the connection more visible.
+#I could see a senario where a user adds components and then doesn't
+#see which signals to connect and where they should be connected.
+#By exporting the dependancy and then using the has_componet method,
+#We can show in-editor what componets this component works with but 
+#not crash the game if the component reference is not needed and not referenced.
+#It might be slightly faster as well.
+#@export var invincibility_component: InvincibilityComponent
+
 signal took_damage(value: int)
+
+# Candidate for Component Class or global helper function
+# func has_component(component) -> bool:
+# 	if component:
+# 		return true
+# 	return false
+
+# Candidate for Component Class or global helper function
+# func set_component_property(component, property: String, value, deferred: bool = true):
+# 	if has_component(component) and property in component:
+# 		if deferred: 
+# 			component.set_deferred(property, value)
+# 		else: 
+# 			component.set(property, value)
 
 func _on_HurtBox_body_entered(body):
 	if body.is_in_group(hurt_group):
-		emit_signal("took_damage", body.damage)
+		took_damage.emit(body.damage)
+#		set_component_property(invincibility_component, is_invulnerable, true, false)
 
-func _on_invincibility_component_invincibility_toggled(is_invincible):
-	set_monitoring(not is_invincible)
+func _on_health_component_died():
+	set_deferred("monitoring", false)
--- project/src/components/InvincibilityComponent.gd
+++ project/src/components/InvincibilityComponent.gd
@@ -1,18 +1,42 @@
 extends Node
 class_name InvincibilityComponent
 
-@export var invincibility_duration := 2.0
+@export var invulnerable_duration := 5.0
+@export var hurt_box_component: HurtBox
 
 @onready var timer: Timer = $Timer
 
-signal invincibility_toggled(is_invincible)
+@onready var is_invulnerable: bool = false:
+	get:
+		print("get is_invulenerable: " + str(is_invulnerable))
+		return is_invulnerable
+	set(value):
+		print("set is_invulnerable: " + str(value))
+		if value == true:
+			set_component_property(hurt_box_component, "monitoring", false)
+			timer.start(invulnerable_duration)
+			is_invulnerable = value
+		else:
+			is_invulnerable = value
 
-func enable_invincibility():
-	emit_signal("invincibility_toggled", true)
-	timer.start(invincibility_duration)
+# Candidate for Component Class or global helper function
+func has_component(component) -> bool:
+	if component:
+		return true
+	
+	return false
+
+# Candidate for Component Class or global helper function
+func set_component_property(component, property: String, value, deferred: bool = true):
+	if has_component(component) and property in component:
+		if deferred: 
+			component.set_deferred(property, value)
+		else: 
+			component.set(property, value)
 
 func _on_Timer_timeout():
-	emit_signal("invincibility_toggled", false)
+	is_invulnerable = false
+	set_component_property(hurt_box_component, "monitoring", true)
 
-func _on_hurt_box_took_damage(value):
-	enable_invincibility()
+func _on_hurt_box_took_damage(_value):
+	is_invulnerable = true
