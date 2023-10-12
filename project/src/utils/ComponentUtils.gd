5b3fe1417cd2215d2790483b0b459efde978a1ce
loteque <andrewandroid@gmail.com>
2023-10-12 04:31:28 +0200

    add component utils class

---
+++ project/src/utils/ComponentUtils.gd
@@ -0,0 +1,15 @@
+class_name ComponentUtils
+extends Node
+
+func has_component(component) -> bool:
+	if component:
+		return true
+
+	return false
+
+func set_component_property(component, property: String, value, deferred: bool = true):
+	if has_component(component) and property in component:
+		if deferred: 
+			component.set_deferred(property, value)
+		else: 
+			component.set(property, value)
