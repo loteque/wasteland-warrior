a04fb66090e9123ebff7bd28549d258afaf70faa
loteque <andrewandroid@gmail.com>
2023-11-04 09:44:53 -0700

    fix a duplicated varibale I introduced.

--- project/src/AimOrigin.gd
+++ project/src/AimOrigin.gd
@@ -6,13 +6,12 @@ extends Marker2D
 @export var update_period := 15
 
 @onready var target: Node2D = get_parent()
-@onready top_level = true
 
-func follow(follow_target: Node2D):
+func _follow(follow_target: Node2D):
 	transform.origin = follow_target.transform.origin
 
 func _ready():
 	top_level = true # Ensures it's not affected by parent scaling
 
 func _physics_process(_delta: float) -> void:
-	follow(target)
+	_follow(target)
